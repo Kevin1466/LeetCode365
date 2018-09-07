931844e6fe249baf5df7e13cdd6e97692e763761
Kevin1466 <kevin4012@outlook.com>
2018-09-07 17:25:10 +0800

    九月七号，简单难度

--- september/seventh/NthDigit.java
+++ september/seventh/NthDigit.java
@@ -1,11 +1,5 @@
 package september.seventh;
 
-import com.sun.org.apache.regexp.internal.RE;
-
-import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.List;
-
 /**
  * Find the nth digit of the infinite integer sequence 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, ...
  *
@@ -14,21 +8,45 @@ import java.util.List;
  */
 public class NthDigit {
     public static void main(String[] args) {
-        System.out.print(findNthDigit(15));
+        System.out.print(findNthDigit(11));
     }
 
     public static int findNthDigit(int n) {
-        int number = 0;
-        List<Character> characterList = new LinkedList<>();
+        /**
+         * 1-9，一次贡献1个digit，一共9个
+         * 10-99，一次贡献2个digit，一共90*2个
+         * 100-999，一次贡献3个digit，一共900*3个
+         * 1000-9999, n = 1002
+         *
+         * 若n==99,
+         */
+        int digit = 0;
+        int count = 0;
         while (true) {
-            number ++;
-            for (Character ch : String.valueOf(number).toCharArray()) {
-                if (characterList.size() < n) {
-                    characterList.add(ch);
-                } else {
-                    return ((LinkedList<Character>) characterList).getLast() - '0';
-                }
+            int digits = (int) (9 * Math.pow(10, digit) * (++digit)); // 9, 180, 270
+            if (count + digits > n) {
+                int d = n - count; // 再数4个digit
+                int i = d / digit; // 4个digit就是i个数
+                int base = (int) Math.pow(10, digit-1);
+                int y = d - (i * digit); // 在第i个数字上，取第y个digit
+                return String.valueOf(base + i).charAt(y-1) - '0';
+            } else {
+                count += digits;
             }
         }
+
+       /* int number = 0;
+        String strNumber;
+        int charLength = 0;
+        while (true) {
+            number ++;
+            strNumber = String.valueOf(number);
+            int numLen = strNumber.length();
+            if (charLength + numLen < n) {
+                charLength += numLen;
+            } else {
+                return strNumber.charAt(n-charLength-1) - '0';
+            }
+        }*/
     }
 }
